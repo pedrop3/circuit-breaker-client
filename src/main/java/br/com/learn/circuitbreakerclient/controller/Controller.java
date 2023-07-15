65f879c45e1cf683fe3a0d26b3166e228152688c
Pedro Santos <pedro_henriquems321@hotmail.com>
2023-07-15 16:27:25 -0300

    Improving

--- src/main/java/br/com/learn/circuitbreakerclient/controller/Controller.java
+++ src/main/java/br/com/learn/circuitbreakerclient/controller/Controller.java
@@ -3,7 +3,7 @@ package br.com.learn.circuitbreakerclient.controller;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,14 +18,14 @@ public class Controller {
     private final AutomationService automationService;
 
     @GetMapping(path = "/{patientId}" )
-    public List<AutomationResponse> getBin(
+    public ResponseEntity<List<AutomationResponse>> findAutomationByPatientId(
             @PathVariable Long patientId
             )  {
                 
-        return  automationService.findByPatientId(patientId)
+        return  ResponseEntity.ok(automationService.findByPatientId(patientId)
                     .stream()
                     .map(AutomationResponse::of)
-                    .collect(Collectors.toList());
+                    .collect(Collectors.toList()));
     }
 
 }
